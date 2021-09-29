04aa41c35208502e5ef73f09c18844afa055fac2
rino a <88568741+zyfca@users.noreply.github.com>
2021-09-29 21:50:28 +0700

    Add files via upload
    Buka bagian hero image

---
+++ Tes HTML/css3.js
@@ -0,0 +1,23 @@
+
+// const filter = document.querySelector(".filter");
+// const isi = document.querySelector(".isi");
+
+
+
+
+// const a = filter.addEventListener("mouseover", function () {
+//     filter.classList.add("muter");
+//     const b = setInterval(function () {
+//         filter.classList.add("muter");
+//     }, 1);
+//     setInterval(function () {
+//         filter.classList.remove("muter");
+//     }, 800);
+
+//     filter.addEventListener("mouseleave", () => {
+//         clearInterval(b);
+//     })
+// })
+
+
+
---
+++ Tes HTML/script.js
@@ -0,0 +1,5 @@
+const iklan = document.querySelector(".x");
+const banner = document.querySelector(".banner")
+iklan.addEventListener("click", () => {
+    banner.innerHTML = "";
+})
